56f8b23ce20ae482ad46da0f6bf6e167592810e4
Jessica <jpbonson@gmail.com>
2017-12-20 18:08:31 -0200

    improved spider for techcrunch

--- webscraper/webscraper/spiders/techcrunch.py
+++ webscraper/webscraper/spiders/techcrunch.py
@@ -1,4 +1,5 @@
 import scrapy
+from scrapy.utils.markup import remove_tags
 
 
 class TechcrunchSpider(scrapy.Spider):
@@ -16,13 +17,17 @@ class TechcrunchSpider(scrapy.Spider):
         authors = response.xpath('//item/creator/text()').extract()
         dates = response.xpath('//item/pubDate/text()').extract()
         links = response.xpath('//item/link/text()').extract()
+        description = response.xpath('//item/description/text()').extract()
+        categories = response.xpath('//item/category/text()').extract()
 
-        for item in zip(titles, authors, dates, links):
+        for item in zip(titles, authors, dates, links, description, categories):
             scraped_info = {
                 'title': item[0],
                 'author': item[1],
                 'publish_date': item[2],
-                'link': item[3]
+                'link': item[3],
+                'description': remove_tags(item[4]).replace(u'&nbsp;', u''),
+                'categories': [item[5]]  # TODO: fix this, should store all categories
             }
 
             yield scraped_info
