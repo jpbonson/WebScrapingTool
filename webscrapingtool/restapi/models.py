f0056d21d7ab98d13d60350ff6aed21df521f510
Jessica <jpbonson@gmail.com>
2017-12-20 13:19:02 -0200

    created model for article

--- webscrapingtool/restapi/models.py
+++ webscrapingtool/restapi/models.py
@@ -12,10 +12,21 @@ class Outlet(models.Model):
 
 
 class Author(models.Model):
-    outlet = models.ForeignKey(Outlet, related_name='reviews', on_delete=models.CASCADE)
+    outlet = models.ForeignKey(Outlet, related_name='author_outlets', on_delete=models.CASCADE)
     name = models.CharField(max_length=255)
     email = models.CharField(max_length=255)
     profile_page = models.CharField(max_length=255, null=True)
+
+    def __repr__(self):
+        return "%s(%r)" % (self.__class__, self.__dict__)
+
+
+class Article(models.Model):
+    outlet = models.ForeignKey(Outlet, related_name='article_outlets', on_delete=models.CASCADE)
+    author = models.ForeignKey(Author, related_name='article_authors', on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    content = models.TextField(blank=True, null=True)
+    publication_date = models.DateField()
 
     def __repr__(self):
         return "%s(%r)" % (self.__class__, self.__dict__)
