ca57a9566278a1a763c636711e846a17da9b4fb9
Jessica <jpbonson@gmail.com>
2017-12-20 13:07:37 -0200

    added field profile_page to authors

--- webscrapingtool/restapi/models.py
+++ webscrapingtool/restapi/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class Outlet(models.Model):
     name = models.CharField(max_length=255)
     website = models.CharField(max_length=255)
-    description = models.TextField(blank=True)
+    description = models.TextField(blank=True, null=True)
 
     def __repr__(self):
         return "%s(%r)" % (self.__class__, self.__dict__)
@@ -15,6 +15,7 @@ class Author(models.Model):
     outlet = models.ForeignKey(Outlet, related_name='reviews', on_delete=models.CASCADE)
     name = models.CharField(max_length=255)
     email = models.CharField(max_length=255)
+    profile_page = models.CharField(max_length=255, null=True)
 
     def __repr__(self):
         return "%s(%r)" % (self.__class__, self.__dict__)
