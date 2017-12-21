9d65868642751eed3cf28beeb3c01159805b595e
Jessica <jpbonson@gmail.com>
2017-12-21 19:27:24 -0200

    removed static config

--- webscrapingtool/webscrapingtool/settings.py
+++ webscrapingtool/webscrapingtool/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
+    # 'django.contrib.staticfiles',
     'rest_framework',
     'restapi',
 ]
@@ -132,7 +132,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
-STATIC_URL = '/static/'
+# STATIC_URL = '/static/'
+# STATIC_ROOT = '/var/www/example.com/static/'
 
 REST_FRAMEWORK = {
     'DEFAULT_THROTTLE_CLASSES': (
